90e836ef37ac515f058f1ae503d73d8349505208
Swexbe <erik@erikjohnsson.se>
2020-01-27 15:24:54 +0100

    Co-authored-by: Joakim Hulthe <hulthe@student.chalmers.se> Co-authored-by: Sorchar <gusmathfo@student.gu.se>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+mod types;
+
+use std::mem::size_of;
+
+use crate::types::*;
+
+/*
+Thing = Var1 Int Int | Var2 Float
+
+all_types: HashMap<String, Type>;
+*/
+
 fn main() {
-    println!("Hello, world!");
+    let example: Type = Type::Sum(vec![
+        ("Var1".into(), vec![Type::Int]),
+        ("Var2".into(), vec![]),
+    ]);
+
+    println!("Size of {:#?}: {}", example, example.size_of());
 }
---
+++ src/types.rs
@@ -0,0 +1,42 @@
+use bincode::{deserialize, serialize};
+use std::cmp;
+use std::mem::size_of;
+
+type EnumTag = u32;
+#[derive(Debug)]
+enum Type {
+    Int,
+    Sum(Vec<(String, Vec<Type>)>),
+}
+
+impl Type {
+    fn size_of(&self) -> usize {
+        match self {
+            Type::Int => size_of::<i32>(),
+            Type::Sum(variants) => {
+                size_of::<u32>()
+                    + variants
+                        .iter()
+                        .map(|(_, ts)| ts.iter().map(|t| t.size_of()).sum())
+                        .fold(0, cmp::max)
+            }
+        }
+    }
+
+    fn parse(&self, bytes: &[u8]) -> bincode::Result<()> {
+        assert_eq!(self.size_of(), bytes.len());
+
+        match self {
+            Type::Int => {
+                let value = deserialize::<i32>(bytes);
+                eprintln!("int: {}", value);
+            }
+            Type::Sum(variants) => {
+                let tag = deserialize::<EnumTag>(bytes[..]);
+                eprintln!("int: {}", value);
+            }
+        }
+
+        Ok(())
+    }
+}
