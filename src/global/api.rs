3d2c4d134a1554e5c4d8f2937807459376963a7c
Joakim Hulthe <joakim@hulthe.net>
2020-02-13 01:04:14 +0100

    Add TODO on global resource manager api

--- src/global/api.rs
+++ src/global/api.rs
@@ -2,6 +2,11 @@ use super::manager::REQUEST_SENDER;
 use super::types::{Request, Response};
 use crossbeam::channel;
 
+// TODO
+// there is no type constraint here on what kind of responses that can be returned for a given
+// response. e.g. A TableCreated response doesn't make sense when requesting access to the type map
+
+/// Send a resource request to the global resource manager
 pub fn send_request(request: Request) -> Response {
     let (response_in, response_out) = channel::unbounded();
     REQUEST_SENDER
