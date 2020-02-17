1bf39d7d6dbdc4dd1805095d71100ceb0c77a3c4
Swexbe <erik@erikjohnsson.se>
2020-02-17 11:10:17 +0100

    String as buffer

--- src/api/tcp_api.rs
+++ src/api/tcp_api.rs
@@ -12,20 +12,27 @@ pub async fn tcp_api(func: fn(&str) -> String, address: String) -> Result<!, Box
             Ok((mut socket, _)) => {
                 tokio::spawn(async move {
                     let (reader, mut writer) = socket.split();
-                    let mut buf: Vec<u8> = vec![];
+                    let mut buf= String::new();
+                    let mut rest= String::new();
                     let mut reader: BufReader<_> = BufReader::new(reader);
 
                     loop {
-                        let n: usize = reader.read_until(b';', &mut buf).await.unwrap();
+                        let n: usize = reader.read_line(&mut buf).await.unwrap();
 
-                        let input = std::str::from_utf8(&buf[..n]).expect("Not valid utf-8");
+                        rest.push_str(&buf.trim_end());
+                        let (result, rest2) = conga(func, rest);
+                        rest = rest2;
 
-                        let result = func(input);
-                        eprintln!("{}", result);
-
-                        buf.drain(..n);
-                        writer.write_all(result.as_bytes()).await.unwrap();
-                        writer.flush();
+                        // TODO: fix for unicode
+                        buf.drain(..buf.len());
+                        match result {
+                            Some(ret) => {
+                                writer.write_all(ret.as_bytes()).await.unwrap();
+                                writer.flush().await.unwrap();
+                            },
+                            None => (),
+                        }
+                        
                     }
                 });
             }
@@ -33,3 +40,41 @@ pub async fn tcp_api(func: fn(&str) -> String, address: String) -> Result<!, Box
         }
     }
 }
+
+// CONGA FIX EVERYTHING
+fn conga(func: fn(&str) -> String, stmt: String) -> (Option<String>, String){
+    let mut in_string = false;
+    let mut result = vec![];
+    let mut lasti = 0;
+    let chars = stmt.chars().enumerate();
+
+    for (i,ch) in chars {
+        // TODO: Handle escape characters
+        if ch == '"' {
+            in_string = !in_string;
+        }
+
+        if ch == ';' && !in_string {
+            let q = &stmt[lasti..=i];
+            result.push(func(q));
+            lasti = i + 1;
+        }
+    }
+
+    let mut rest = String::new();
+    let mut ret = None;
+
+
+    if lasti != (stmt.len() - 1) {
+        rest = String::from(&stmt[lasti..stmt.len()]);
+    }
+
+    if !result.is_empty() {
+        ret = Some(result.join("\n"));
+    }
+
+    (ret, rest)
+}
+
+
+
