53cda5e78cc751e2932b4a0e9aadd64055821f97
Johannes Mattsson <karlmattsson96@gmail.com>
2020-02-18 10:15:01 +0100

    Start of full slice

--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,48 @@ mod api;
 
 use api::tcp_api::tcp_api;
 use std::error::Error;
+use crate::ast::Stmt;
 
 #[tokio::main]
 async fn main() -> Result<!, Box<dyn Error>> {
-    tcp_api(echo_ast, "127.0.0.1:5432".to_string()).await
+    tcp_api(execute_query, "127.0.0.1:5432".to_string()).await
+}
+
+fn execute_query(input: &str) -> String {
+    // 1. parse
+    use crate::grammar::StmtParser;
+    use crate::global::*;
+
+    let result: Result<Stmt, _> = StmtParser::new().parse(&input);
+    let ast = match result {
+        Ok(ast) => ast,
+        Err(e) => return format!("{:#?}\n", e),
+    };
+
+    // 2. determine resources
+    let resource_request = pre_typechecker::get_table_permissions(&ast);
+
+    // 3. acquire resources
+    let request = Request::AcquireResources(resource_request);
+    let response = send_request(request);
+    let mut resources = match response {
+        Response::AcquiredResources(resources) => resources,
+        Response::NoSuchTable(name) => return format!("No such table: {}\n", name),
+        _ => unreachable!("Invalid reponse from global::send_request"),
+    };
+    let guard = resources.take();
+
+    // 4. typecheck
+    match typechecker::check_stmt(&ast, &guard) {
+        Ok(()) => {},
+        Err(e) => return format!("{:#?}\n", e),
+    }
+
+    // 5. TODO: Maybe convert ast to some internal representation of a query (See EXPLAIN in postgres/mysql)
+
+    // 6. TODO: Execute query
+
+    return String::from("Not implemented");
 }
 
 fn echo_ast(input: &str) -> String {
