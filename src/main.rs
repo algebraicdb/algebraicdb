9fbef06f7bb54ce462e27833aaed38f39bab24a6
Joakim Hulthe <joakim@hulthe.net>
2020-02-27 13:31:20 +0100

    Remove unused #[cfg(...)]

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,5 @@ use std::error::Error;
 
 #[tokio::main]
 async fn main() -> Result<!, Box<dyn Error>> {
-    #[cfg(features = "wrapper")]
-    unimplemented!();
-
     create_tcp_server("127.0.0.1:5432").await
 }
