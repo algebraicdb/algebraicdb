aeaa4ec5257235c8417f44c526cbceeb3cc3c0d4
feffe <feffe@feffe.dev>
2020-02-29 13:58:32 +0100

    Create table translation

--- src/psqlwrapper/translator/create_table.rs
+++ src/psqlwrapper/translator/create_table.rs
@@ -1,19 +1,26 @@
 use crate::table::*;
 use crate::ast::*;
-use crate::types::{TypeMap, TypeId};
+use crate::types::{TypeMap, TypeId, Type};
 
 
 pub fn translate_create_table(create_table: CreateTable, schema: &Schema, typemap: &TypeMap) -> String  {
-    return format!("CREATE TABLE {}", translate_table(schema, typemap))
+    return format!("CREATE TABLE {};", translate_table(schema, typemap))
 }
 
 fn translate_table(schema: &Schema, typemap: &TypeMap)-> String {
-    schema.columns.iter().fold(String::from(""), )
+    schema.columns.iter().map(|x| col_to_string(x, typemap)).collect::<Vec<String>>().join(",")
 }
 
-fn col_to_string(col: Vec<(String, TypeId)>) -> String {
+fn col_to_string(col: &(String, TypeId), typemap: &TypeMap) -> String {
+    format!("{} {} NOT NULL", col.0, translate_typeid(&col.1, typemap))
+}
+
+fn translate_typeid(tid: &TypeId, typemap: &TypeMap) -> String {
+    match typemap.get_by_id(*tid) {
+        Type::Bool => String::from("BOOLEAN"),
+        Type::Double => String::from("float64"),
+        Type::Integer => String::from("INT"),
+        Type::Sum(_) => String::from("json"),
+    }
     
-}
-
-fn translate_typeid(tid: TypeId, typemap: &TypeMap) -> String {
 }
