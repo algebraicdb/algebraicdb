08a9c4d834723d2d0470d8cf6d2baacc86d774fb
Swexbe <erik@erikjohnsson.se>
2020-03-26 15:55:21 +0100

    Fixed selecting variants

--- src/psqlwrapper/translator/select.rs
+++ src/psqlwrapper/translator/select.rs
@@ -1,4 +1,5 @@
 use crate::ast::{Expr, Select, SelectFrom, WhereClause, WhereItem};
+use crate::pattern::Pattern;
 use crate::types::Value;
 
 pub fn translate_select(select: &Select) -> String {
@@ -6,10 +7,10 @@ pub fn translate_select(select: &Select) -> String {
         "SELECT {} {} {};",
         translate_item(&select.items),
         translate_from(&select.from),
-        match &select.where_clause{
+        match &select.where_clause {
             Some(clause) => translate_where(&clause),
             None => String::new(),
-        } 
+        }
     )
 }
 
@@ -24,8 +25,11 @@ fn translate_from(sfrom: &Option<SelectFrom>) -> String {
 fn translate_item(items: &Vec<Expr>) -> String {
     format!(
         "{}",
-        items.iter().map(|a|
-            translate_expr(&a)).collect::<Vec<String>>().join(",")
+        items
+            .iter()
+            .map(|a| translate_expr(&a))
+            .collect::<Vec<String>>()
+            .join(",")
     )
 }
 
@@ -48,14 +52,35 @@ fn translate_expr(expr: &Expr) -> String {
     }
 }
 
+fn translate_pattern(path: &String, pattern: &Pattern) -> String {
+    match pattern {
+        Pattern::Ignore => format!("{} IS NOT NULL", path),
+        Pattern::Variant {
+            name,
+            namespace: _,
+            sub_patterns,
+        } => sub_patterns
+            .iter()
+            .enumerate()
+            .map(|(n, pat)| translate_pattern(&format!("{} -> {} -> '{}'", path, n, name), pat))
+            .collect::<Vec<String>>()
+            .join(" AND "),
+        _ => unimplemented!(),
+    }
+}
+
 fn translate_where(clause: &WhereClause) -> String {
-    let items_string: Vec<String> = clause.items.iter().map(|x| translate_where_item(x)).collect();
-    format!("WHERE {}", items_string.join(","))
+    let items_string: Vec<String> = clause
+        .items
+        .iter()
+        .map(|x| translate_where_item(x))
+        .collect();
+    format!("WHERE {}", items_string.join(" AND "))
 }
 
 fn translate_where_item(witem: &WhereItem) -> String {
     match witem {
         WhereItem::Expr(expr) => translate_expr(expr),
-        _ => unimplemented!(),
+        WhereItem::Pattern(name, pattern) => translate_pattern(name, pattern),
     }
 }
