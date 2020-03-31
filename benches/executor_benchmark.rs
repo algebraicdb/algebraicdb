18f5ca8adc176a3c0d89e0e6e56d2fe5fc69744d
Joakim Hulthe <joakim@hulthe.net>
2020-03-31 19:36:33 +0200

    Remove unused import

--- benches/executor_benchmark.rs
+++ benches/executor_benchmark.rs
@@ -1,7 +1,7 @@
 use algebraicdb::ast::SelectFrom;
 use algebraicdb::executor::dbms::execute_select_from;
 use algebraicdb::local::types::{Resource, ResourcesGuard};
-use algebraicdb::table::{Row, Schema, Table};
+use algebraicdb::table::{Schema, Table};
 use algebraicdb::types::{BaseType, TypeMap, Value};
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
